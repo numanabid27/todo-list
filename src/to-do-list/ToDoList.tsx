dc65fcb39fd11a79dec2afc5fab28d7085305365
numanabid27 <numanabid27@gmail.com>
2023-11-03 02:46:49 +0500

    data store in local storage

--- src/to-do-list/ToDoList.tsx
+++ src/to-do-list/ToDoList.tsx
@@ -1,31 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./toDoList.module.css";
 
 export const ToDoList = () => {
-  const [value, setValue] = useState<any>('');
+  const [value, setValue] = useState<any>("");
   const [isValue, setIsValue] = useState<any>([]);
 
-  const handleSubmit = (e:any) =>{
+  const handleSubmit = (e: any) => {
     e.preventDefault();
-    setIsValue((prevState:any) => [...prevState, value]);
-    setValue(''); 
-    console.log("...", isValue)
-  }
+    setIsValue((prevState: any) => [...prevState, value]);
+    setValue("");
+    console.log("...", isValue);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("isValue", JSON.stringify(isValue));
+  }, [isValue]);
 
   return (
     <>
       <div className={styles.todo_sec}>
         <form onSubmit={handleSubmit}>
           <label>User Name </label>
-          <input type="text" value={value} onChange={(e)=>{setValue(e.target.value)}} required/>
+          <input
+            type="text"
+            value={value}
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+            required
+          />
           <button type="submit">Add Value</button>
         </form>
       </div>
       <ul>
-          {isValue.map((item:any, index:any) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
+        {isValue.map((item: any, index: any) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </>
   );
 };
